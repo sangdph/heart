d5c86bab00cfb4519c6f7085b7176828c053ef8f
sangdph <sangdph.developer@gmail.com>
2022-11-13 18:12:44 +0700

    95%

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var miniHearts = document.getElementById("mini-hearts");
 var zIndex = -900;
+
 function createHeart(x,y, zIndex){
+    var size = (Math.floor(Math.random() * 300)+100)/100;
+    
+    var red = Math.floor(Math.random() * 50)+Math.floor(Math.random() * 4)+200;
+    var green = Math.floor(Math.random() * 150)+Math.floor(Math.random() * 30)+20;
+    var blue = Math.floor(Math.random() * 95)+Math.floor(Math.random() * 5)+100;
     const miniHeart = `
-        <div class="mini-heart" style="top: ${y}px; left: ${x}px; z-index: ${zIndex}"></div>
+        <div class="mini-heart" style="top: ${y}px; left: ${x}px; z-index: ${zIndex}; --red: ${red}; --green: ${green}; --blue: ${blue}; --size: ${size}"></div>
     `;
     return miniHearts.insertAdjacentHTML("beforeend",miniHeart);
 }
